95593faa1f0a2b45ce9e64d54c629ff7184957e1
RekhaVaghela <rekha.vaghela@hotmail.co.uk>
2018-08-28 20:58:35 +0100

    illegal error keeps popping up

---
+++ src/main/java/com/qa/quickstart/Individual_Project/model/Account.java
@@ -0,0 +1,28 @@
+package com.qa.quickstart.Individual_Project.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Account {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+	
+
+}
